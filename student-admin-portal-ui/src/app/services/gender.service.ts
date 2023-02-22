00e757f35e0679e72c29340ec15765ab5c755689
Jordan Aletraris <jordanaletraris@gmail.com>
2023-02-21 20:50:59 -0500

    Small changes to front end code. Trying to figure out weird formatting issues

--- student-admin-portal-ui/src/app/services/gender.service.ts
+++ student-admin-portal-ui/src/app/services/gender.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Gender } from '../models/api-models/gender.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenderService {
 
-  private baseApiUrl = 'https://localhost:44352';
+  private baseApiUrl = environment.baseApiUrl;
 
   constructor(private httpClient: HttpClient) { }
 
